9ba8bce16043b5b592a329c5eb8ce6cd9abf022c
Luis Rivas <69378112+Lnrivas@users.noreply.github.com>
2023-04-03 21:02:11 -0700

    uploading part 3(creation of database)

---
+++ app.py
@@ -0,0 +1,12 @@
+from db_operations import db_operations
+
+db_ops = db_operations("playlist.db")
+
+# db_ops.create_user_table() # RUN ONLY ONCE, THEN COMMENT OUT
+# db_ops.create_driver_table() # RUN ONLY ONCE, THEN COMMENT OUT
+# db_ops.create_vehicle_table() # RUN ONLY ONCE, THEN COMMENT OUT
+# db_ops.create_trip_table() # RUN ONLY ONCE, THEN COMMENT OUT
+# db_ops.create_rating_table() # RUN ONLY ONCE, THEN COMMENT OUT
+
+# deconstruct at end
+db_ops.destructor()
---
+++ db_operations.py
@@ -0,0 +1,88 @@
+#import MySQL
+import mysql.connector
+
+class db_operations():
+
+    # constructor with connection path to database
+    def __init__(self, conn_path):
+        #Make Connection
+        self.connection = mysql.connector.connect(host = "localhost",
+        user = "root",
+        password = "cpsc-408",
+        auth_plugin = 'mysql_native_password',
+        database = "RideShare")
+        #create cursor object
+        self.cursor = self.connection.cursor()
+        print("connection made")
+
+    def create_user_table(self):
+        query = '''
+        CREATE TABLE User(
+            UserID INT PRIMARY KEY NOT NULL,
+            Name VARCHAR(50) NOT NULL,
+            Email VARCHAR(100) NOT NULL UNIQUE,
+            Phone VARCHAR(15) NOT NULL UNIQUE,
+            Password VARCHAR(100) NOT NULL,
+            UserType ENUM('Rider', 'Driver') NOT NULL
+        );
+        '''
+        self.cursor.execute(query)
+        print("User table Created")
+
+    def create_driver_table(self):
+        query = '''
+        CREATE TABLE Driver(
+            DriverID INT PRIMARY KEY NOT NULL,
+            Driver_mode BOOLEAN NOT NULL,
+            Rating FLOAT NOT NULL
+        );
+        '''
+        self.cursor.execute(query)
+        print("Driver table Created")
+
+    def create_vehicle_table(self):
+        query = '''
+        CREATE TABLE Vehicle(
+            VehicleID INT PRIMARY KEY NOT NULL,
+            Make VARCHAR(50) NOT NULL,
+            Model VARCHAR(50) NOT NULL,
+            Year INT NOT NULL,
+            License_plate VARCHAR(10) NOT NULL UNIQUE
+        );
+        '''
+        self.cursor.execute(query)
+        print("Vehicle table Created")
+
+    def create_trip_table(self):
+        query = '''
+        CREATE TABLE Trip(
+            TripID INT PRIMARY KEY NOT NULL,
+            RiderID INT NOT NULL,
+            DriverID INT NOT NULL,
+            Pickup_location VARCHAR(200) NOT NULL,
+            Dropoff_location VARCHAR(200) NOT NULL,
+            Pickup_time DATETIME NOT NULL,
+            Dropoff_time DATETIME NOT NULL,
+            Fare DECIMAL(7,2) NOT NULL
+        );
+        '''
+        self.cursor.execute(query)
+        print("Trip table Created")
+
+    def create_rating_table(self):
+        query = '''
+        CREATE TABLE Rating(
+            RatingID INT PRIMARY KEY NOT NULL,
+            TripID INT NOT NULL,
+            RiderID INT NOT NULL,
+            DriverID INT NOT NULL,
+            Rating_score INT NOT NULL CHECK(Rating_score BETWEEN 1 AND 5),
+            Timestamp DATETIME NOT NULL
+        );
+        '''
+        self.cursor.execute(query)
+        print("Rating table Created")
+
+    # destructor that closes connection to database
+    def destructor(self):
+        self.connection.close()

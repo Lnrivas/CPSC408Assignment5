ec9588a3ec34085012f821a67cb5a1549e0860cb
Luis Rivas <69378112+Lnrivas@users.noreply.github.com>
2023-04-06 10:40:13 -0700

    no longer creates a vehicle table

--- app.py
+++ app.py
@@ -5,7 +5,6 @@ db_ops = db_operations()
 
 # db_ops.create_user_table() # RUN ONLY ONCE, THEN COMMENT OUT
 # db_ops.create_driver_table() # RUN ONLY ONCE, THEN COMMENT OUT
-# db_ops.create_vehicle_table() # RUN ONLY ONCE, THEN COMMENT OUT
 # db_ops.create_trip_table() # RUN ONLY ONCE, THEN COMMENT OUT
 # db_ops.create_rating_table() # RUN ONLY ONCE, THEN COMMENT OUT
 
